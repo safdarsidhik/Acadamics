1d897c83690f1666636472fc67166ff98f6e6154
safdarsidhik <safdarsidhik@gmail.com>
2025-08-11 21:00:07 +0530

    reverse a number

--- Python/reverse a number.py
+++ Python/reverse a number.py
@@ -14,3 +14,32 @@ print(rev)
 n = input("Enter a number: ")
 rev = n[::-1]
 print(rev)
+
+#3.
+def reversDigits(n):
+
+    s = str(n)
+
+    s = list(s)
+    s.reverse()
+    s = ''.join(s)
+    n = int(s)
+    return n
+
+if __name__ == "__main__":
+
+    num = int(input("Enter a number: "))
+    print(reversDigits(num))
+
+#4.
+def reverseDigits(n, revNum, basePos):
+    if n > 0:
+        reverseDigits(n // 10, revNum, basePos)  
+        revNum[0] += (n % 10) * basePos[0]       
+        basePos[0] *= 10                         
+
+n = 4562
+revNum = [0]  
+basePos = [1]  
+reverseDigits(n, revNum, basePos)
+print(revNum[0])
